5c402a2a8f353306dd4f9afc0dcffe09e5395323
ShubhamSoitkarNeosoft <shubham.soitkar@neosoftmail.com>
2022-06-22 09:28:52 +0530

    delete tweet functionality added

--- twitter/blog/urls.py
+++ twitter/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('comment/<int:pk>',views.comment_view, name = 'comment'),
     path('post-like/',views.like_post, name = 'like-post'),
     path('update-post/<int:pk>',views.update_post, name = 'update-post'),
+    path('delete-post/<int:pk>',views.delete_post, name = 'delete-post'),
     
 ]
